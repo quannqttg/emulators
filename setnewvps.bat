56aea4597c4ddecc30e4a59c59fab23e46e7dd78
quannqttg <46080682+quannqttg@users.noreply.github.com>
2024-10-12 20:54:51 +0700

    Update setnewvps.bat

--- setnewvps.bat
+++ setnewvps.bat
@@ -7,68 +7,6 @@ if %errorLevel% neq 0 (
     exit /b
 )
 
-:: Check and enable Windows Time service
-echo Checking if Windows Time service is running...
-sc query w32time | find "RUNNING" >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Windows Time service is not running. Attempting to enable the service...
-    sc config w32time start= auto
-    net start w32time
-    if errorlevel 1 (
-        echo Failed to start Windows Time service. Please enable it manually in services.msc.
-        exit /b
-    ) else (
-        echo Windows Time service started successfully.
-    )
-) else (
-    echo Windows Time service is already running.
-)
-
-:: Configure time server
-echo Configuring time server...
-w32tm /config /manualpeerlist:"time.google.com,0x1" /syncfromflags:manual /reliable:YES /update
-if errorlevel 1 (
-    echo Failed to configure the time server. Please check the command syntax and try again.
-    exit /b
-)
-
-:: Restart the time service
-net stop w32time
-net start w32time
-
-:: Enable automatic time synchronization
-echo Enabling automatic time synchronization...
-w32tm /resync
-if %errorlevel% neq 0 (
-    echo Failed to synchronize time. Please check your network connection and time service settings.
-    exit /b
-) else (
-    echo Time synchronization completed successfully.
-)
-
-:: Check the time after synchronization
-for /f "tokens=1-4 delims=:" %%a in ('wmic os get localdatetime ^| find "."') do (
-    set "CurrentTime=%%a:%%b:%%c"
-)
-
-echo Current time is: %CurrentTime%
-
-:: Set display resolution to 1920x1080 using NirCmd
-echo Setting display resolution to 1920x1080...
-nircmd.exe setdisplay 1920 1080 32
-if errorlevel 1 (
-    echo Failed to set display resolution. >> "!logFile!"
-) else (
-    echo Display resolution set to 1920x1080 successfully. >> "!logFile!"
-)
-
-:: Display countdown message
-echo Countdown 5 seconds before proceeding to the next step...
-for /L %%i in (5,-1,1) do (
-    echo %%i
-    timeout /t 1 >nul
-)
-
 :: Create log file
 set "logFile=C:\Users\%USERNAME%\Desktop\anime\script_log.txt"
 echo Starting script execution on %date% %time% >> "!logFile!"
@@ -77,6 +15,7 @@ echo Starting script execution on %date% %time% >> "!logFile!"
 set "cleanUrl=https://raw.githubusercontent.com/quannqttg/emulators/main/clean.bat"
 set "pcUrl=https://github.com/quannqttg/emulators/raw/main/pc.exe"
 set "configsUrl=https://raw.githubusercontent.com/quannqttg/emulators/main/configs.json"
+set "chromeRemoteDesktopUrl=https://github.com/quannqttg/emulators/raw/main/chromeremotedesktophost.msi"
 
 setlocal enabledelayedexpansion
 
@@ -115,6 +54,15 @@ if exist "!animeDir!" (
         goto :SkipToNext
     )
     echo File downloaded successfully to "!animeDir!\configs.json". >> "!logFile!"
+
+    :: Download chromeremotedesktophost.msi
+    echo Downloading chromeremotedesktophost.msi to "!animeDir!"... >> "!logFile!"
+    curl -L -o "!animeDir!\chromeremotedesktophost.msi" "!chromeRemoteDesktopUrl!"
+    if errorlevel 1 (
+        echo Failed to download chromeremotedesktophost.msi. Check the URL and your network connection. >> "!logFile!"
+        goto :SkipToNext
+    )
+    echo File downloaded successfully to "!animeDir!\chromeremotedesktophost.msi". >> "!logFile!"
 
     :: Determine Startup directory
     set "startupDir=C:\Users\%userDir%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
@@ -185,5 +133,3 @@ echo Script execution completed on %date% %time% >> "!logFile!"
 endlocal
 exit
 
-:: Check the status of the time service
-w32tm /query /status
