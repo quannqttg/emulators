ca60c3391654f73659f6dd580a7c45441da173b4
quannqttg <46080682+quannqttg@users.noreply.github.com>
2024-10-14 11:57:22 +0700

    Update set_time_display.bat

--- set_time_display.bat
+++ set_time_display.bat
@@ -7,31 +7,43 @@ echo Starting script execution on %date% %time% >> "!logFile!"
 
 :: Check for Admin privileges
 net session >nul 2>&1
-if %errorLevel% neq 0 (
-    echo Please run as administrator.
+if errorlevel 1 (
+    echo Please run as administrator. >> "!logFile!"
     powershell -command "Start-Process cmd -ArgumentList '/c %~f0' -Verb RunAs"
     exit /b
 )
 
 :: Check and enable Windows Time service
 sc query w32time | find "RUNNING" >nul 2>&1
-if %errorlevel% neq 0 (
-    sc config w32time start= auto >nul 2>&1
-    net start w32time >nul 2>&1
+if errorlevel 1 (
+    echo Windows Time service is not running. Attempting to start it... >> "!logFile!"
+    sc config w32time start= auto >> "!logFile!" 2>&1
+    net start w32time >> "!logFile!" 2>&1
+) else (
+    echo Windows Time service is already running. >> "!logFile!"
 )
 
 :: Configure time server
-w32tm /config /manualpeerlist:"time.google.com,0x1" /syncfromflags:manual /reliable:YES /update >nul 2>&1
+echo Configuring time server... >> "!logFile!"
+w32tm /config /manualpeerlist:"time.google.com,0x1" /syncfromflags:manual /reliable:YES /update >> "!logFile!" 2>&1
 
 :: Restart the time service
-net stop w32time >nul 2>&1
-net start w32time >nul 2>&1
+echo Restarting Windows Time service... >> "!logFile!"
+net stop w32time >> "!logFile!" 2>&1
+net start w32time >> "!logFile!" 2>&1
 
 :: Enable automatic time synchronization
-w32tm /resync >nul 2>&1
+echo Synchronizing time... >> "!logFile!"
+w32tm /resync >> "!logFile!" 2>&1
+
+:: Set the time zone to UTC+7
+echo Setting time zone to UTC+7... >> "!logFile!"
+tzutil /s "SE Asia Standard Time" >> "!logFile!" 2>&1
 
 :: Set display resolution to 1920x1080 using NirCmd
-"C:\Users\%USERNAME%\Desktop\anime\nircmd.exe" setdisplay 1920 1080 32 >nul 2>&1
+echo Setting display resolution to 1920x1080... >> "!logFile!"
+"C:\Users\%USERNAME%\Desktop\anime\nircmd.exe" setdisplay 1920 1080 32 >> "!logFile!" 2>&1
 
+echo Script execution completed on %date% %time% >> "!logFile!"
 endlocal
 exit
