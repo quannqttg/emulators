6f1f3c30f84fd611210d6e61d381e03842bee1ef
quannqttg <46080682+quannqttg@users.noreply.github.com>
2024-10-21 14:15:44 +0700

    Update and rename check_path.bat to create_path.bat

--- create_path.bat
+++ create_path.bat
@@ -4,11 +4,20 @@ setlocal
 :: Define paths for files
 set "userDir=%USERNAME%"
 set "animeDir=C:\Users\%userDir%\Desktop\anime"
-set "logFile=%animeDir%\check_path_shared_folder.txt"
+set "logFile=%animeDir%\create_path.log"
 set "shared_folder_file=%animeDir%\shared_folder.json"
 
 :: Define the path you want to check
 set "pathToCheck=C:\Users\%userDir%\Documents\MuMuSharedFolder"
+
+:: Check if the shared_folder.json file exists and delete it if it does
+if exist "%shared_folder_file%" (
+    del "%shared_folder_file%"
+    echo Deleted existing JSON file: %shared_folder_file%
+    
+    :: Log the deletion message
+    echo [%date% %time%] Deleted existing JSON file: %shared_folder_file% >> "%logFile%"
+)
 
 :: Check if the directory exists
 if exist "%pathToCheck%" (
@@ -16,13 +25,13 @@ if exist "%pathToCheck%" (
     
     :: Create a JSON file with the path in the specified directory
     echo { > "%shared_folder_file%"
-    echo     "shared_folder": "%pathToCheck%" >> "%shared_folder_file%"
+    echo     "shared_folder": "C:\\Users\\%userDir%\\Documents\\MuMuSharedFolder" >> "%shared_folder_file%"
     echo } >> "%shared_folder_file%"
     
     echo JSON file created: %shared_folder_file%
 
     :: Log the success message
-    echo [%date% %time%] JSON file created: %shared_folder_file% >> "%logFile%"
+    echo [%date% %time%] JSON file created: C:\\Users\\%userDir%\\Desktop\\anime\\shared_folder.json >> "%logFile%"
 ) else (
     echo Directory does not exist: %pathToCheck%
 
@@ -31,3 +40,4 @@ if exist "%pathToCheck%" (
 )
 
 endlocal
+exit /b
